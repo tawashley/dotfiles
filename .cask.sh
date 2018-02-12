e86d88f00a00bb9fd19136355f4a50d141341f95
Tris <trisashley@gmail.com>
2018-02-12 20:01:26 +0000

    Update .cask.sh

--- .cask.sh
+++ .cask.sh
@@ -8,6 +8,16 @@
 #
 #
 
+#!/usr/bin/env bash
+#
+# ~/.cask.sh
+#
+# This script installs applications using Homebrew Cask: http://caskroom.io/search
+# Some apps rely on homebrew-versions: https://github.com/caskroom/homebrew-versions
+# `brew tap caskroom/versions` is ran in `.brew.sh`
+#
+#
+
 # Development apps
 echo Installing dev apps…
 brew cask install adobe-creative-cloud
@@ -21,12 +31,22 @@ brew cask install firefox
 brew cask install google-chrome
 
 # Additional apps
-echo Installing some additional apps…
-brew cask install slack
+echo Installing media apps…
 brew cask install spotify
+brew cask install vlc
 
+# Productivity apps
+echo Installing productivity apps…
+brew cask install slack
 brew cask install spectacle
 brew cask install alfred
+brew cask install the-unarchiver
+brew cask install recordit
+brew cask install monolingual
+brew cask install path-finder
+brew cask install appcleaner
+brew cask install caffeine
+brew cask install cyberduck
 
 # Clean up
 echo Cleaning up…
