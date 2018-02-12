a4760de1a9eeacf236f7a4f7fab8449721bf1beb
Tris <trisashley@gmail.com>
2018-02-12 19:43:10 +0000

    Update .brew.sh

--- .brew.sh
+++ .brew.sh
@@ -9,27 +9,19 @@
 
 # Install Homebrew
 echo Installing Homebrew \(gets Xcode cli developer tools too\)…
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 sudo xcodebuild -license
 
 # Tap versions for alternate versions of applications
 brew tap homebrew/versions
 
-
 # Install Git
 echo Installing Git…
 brew install git
 
-
-# Install Node.js - this installs `npm` too, using the recommended installation method
-echo Installing Node…
-# brew install node  Uncomment for latest
-brew install node012
-
 # Install Node Version Manager
 brew install nvm
 echo "source $(brew --prefix nvm)/nvm.sh" >> ~/.bashrc
-
 
 # Install Brew Cask for managing other apps
 echo Installing Brew Cask…
