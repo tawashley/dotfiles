4458f9fa879d092f609a19f7d8c669265dce73c0
Tris <trisashley@gmail.com>
2018-02-12 20:56:17 +0000

    Update .osx.sh

--- .osx.sh
+++ .osx.sh
@@ -6,6 +6,14 @@
 #
 #
 
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=" "
 
