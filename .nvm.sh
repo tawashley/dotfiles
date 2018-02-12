12f7b4c9c2c9cdd353ba7ad36ee892dfeb161745
Tris <trisashley@gmail.com>
2018-02-12 20:14:58 +0000

    Create .nvm.sh

---
+++ .nvm.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# ~/.nvm.sh
+#
+# This file installs recent node version via nvm and set system default version
+#
+#
+
+# Install 8.9.4
+nvm install 8.9.4
+
+# Set as system default version
+nvm alias default 8.9.4
