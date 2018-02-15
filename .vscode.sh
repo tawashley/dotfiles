4570768ae1becd239d1d0ad690607d77c5c0b07c
Tris <trisashley@gmail.com>
2018-02-15 20:26:51 +0000

    feat: Add .vscode file. Install themes and extensions

---
+++ .vscode.sh
@@ -0,0 +1,34 @@
+# extensions
+
+code --install-extension 2gua.rainbow-brackets
+code --install-extension EditorConfig.EditorConfig
+code --install-extension HookyQR.beautify
+code --install-extension Tyriar.sort-lines
+code --install-extension agauniyal.vscode-caniuse
+code --install-extension alefragnani.project-manager
+code --install-extension alexdima.copy-relative-path
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension christian-kohler.path-intellisense
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-complete-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension geddski.macros
+code --install-extension hnw.vscode-auto-open-markdown-preview
+code --install-extension joelday.docthis
+code --install-extension mohsen1.prettify-json
+code --install-extension mrmlnc.vscode-scss
+code --install-extension ms-vscode.atom-keybindings
+code --install-extension ms-vscode.csharp
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension naumovs.color-highlight
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension yzhang.markdown-all-in-one
+
+# themes
+code --install-extension SmukkeKim.theme-setimonokai
+code --install-extension gerane.Theme-Solarized-dark
+code --install-extension kamranmackey.cobalt2-Theme
+code --install-extension zhuangtongfa.Material-theme
