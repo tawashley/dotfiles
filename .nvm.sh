2294360b304e742e8a196d481e08497932a1f6c5
Tristan Ashley <trisashley@gmail.com>
2025-05-22 12:01:59 +0100

    Update .nvm.sh to install node v22 and set this as the default

--- .nvm.sh
+++ .nvm.sh
@@ -9,9 +9,9 @@
 echo "---> nvm setup"
 echo ""
 
-echo "Installing node v16 via nvm"
-nvm install 16.17.0
+echo "Installing node v22 via nvm"
+nvm install 22.15.0
 
 # Set as system default version
-echo "Setting node v16 as system default"
-nvm alias default 16.17.0
+echo "Setting node v22 as system default"
+nvm alias default 22.15.0
