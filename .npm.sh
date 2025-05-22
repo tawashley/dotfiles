43c51d952a86ff10b1b5b60f7c0bae1befe1554e
Tristan Ashley <trisashley@gmail.com>
2025-05-22 15:39:25 +0100

    Don't install any global npm packages

--- .npm.sh
+++ .npm.sh
@@ -9,7 +9,7 @@
 #
 
 echo "---> Global node packages"
-echo ""
+echo "Nothing to install global node package wise"
 
 # Ask for the administrator password upfront
 sudo -v
@@ -17,10 +17,12 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+echo "Nothing to install global node package wise..."
+
 # Uncomment and add your user if you wish
 # npm adduser xxxxxx
 
-npm i browser-sync -g
-npm i nodemon -g
-npm i serve -g
-npm i yarn -g
+#npm i browser-sync -g
+#npm i nodemon -g
+#npm i serve -g
+#npm i yarn -g
