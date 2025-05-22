68187ed6dfe61b510d83696dc3b7220681a28390
Tristan Ashley <trisashley@gmail.com>
2025-05-22 12:03:30 +0100

    Update .vscode.sh (remove all vs code extensions)

--- .vscode.sh
+++ .vscode.sh
@@ -2,9 +2,3 @@
 
 echo "---> VS Code preferences"
 echo ""
-
-echo "Installing VS Code settings sync extension"
-
-code --install-extension Shan.code-settings-sync
-
-echo "Sync settings installed, setup inside VS Code itself"
